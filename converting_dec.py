3e0956372ae017e87274880241229c9a3819ca1a
Slimey274 <briguy030102@gmail.com>
2025-06-18 14:58:22 -0600

    completed convert binary and octal and there tests

--- converting_dec.py
+++ converting_dec.py
@@ -1,22 +1,55 @@
 
 
 def convert_binary(number):
-    '''Conver the Intger number to binary'''
-    return "0b11"
+    '''Convert the Intger number to binary'''
+    if number == 0:
+        return "0"
+    
+    binary_digits = []
+    while number > 0:
+        binary_digits.append(str(number %2))
+        number = number // 2
+
+    binary_digits.reverse()
+    binary_string = ''.join(binary_digits)
+    return "0b" + binary_string
 
 def convert_octal(number):
-    '''COnver the integer number to Octal'''
-    return "o357"
+    '''Convert the integer number to Octal'''
+    if number == 0:
+        return "0"
+    
+    octal_digits = []
+    while number > 0:
+        octal_digits.append(str(number % 8))
+        number = number // 8
+
+    octal_digits.reverse()
+    octal_string = ''.join(octal_digits)
+    return "0o" + octal_string
+
 
 def convert_hex(number):
-    '''Conver the integer number to hexadecimal'''
+    '''Convert the integer number to hexadecimal'''
+    if number == 0:
+        return "0"
+    
+    hex_digits = []
+
+    hex_string = ''
+
+    while number > 0:
+        hex_digits.append(number % 16)
+        number //= 16
+    
+    hex_letters = ['A', 'B', 'C', 'D', 'E', 'F']
     return "0xF3"
 
 def conversion(number):
     '''Dirver function for the convert functions'''
     binary = convert_binary(number)
     octal = convert_octal(number)
-    hex = convert_hex(number)\
+    hex = convert_hex(number)
     
     return binary, octal, hex
 
@@ -27,7 +60,7 @@ def output_numbers(number, binary, octal, hex):
     assert type(octal) == str
     assert type(hex) == str
 
-    print(f"Number: {number:10} Binary: {binary:>10} Octal: {octal:>10} Hex: {hex:>10}")
+    print(f"Number: {number:5} \nBinary: {binary:>5} \nOctal: {octal:>5} \nHex: {hex:>5}")
 
 def get_number():
     '''Obtains number from user'''
@@ -46,7 +79,21 @@ def test_conversions():
     '''Tests for the conversion functions'''
     pass
 
+def test_convert_binary():
+    assert convert_binary(255) == "0b11111111"
+    assert convert_binary(1) == "0b1"
+    assert convert_binary(128) == "0b10000000"
+    print("Binary tests complete")
+
+def test_convert_octal():
+    assert convert_octal(255) == "0o377"
+    assert convert_octal(25) == "0o31"
+    assert convert_octal(128) == "0o200"
+    print("Octal tests pass")
+
 def main():
+    test_convert_binary()
+    test_convert_octal()
     number = get_number()
     print(f'Got a good number {number}')
     binary, octal, hex = conversion(number)
